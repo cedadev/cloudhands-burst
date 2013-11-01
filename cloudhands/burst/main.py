8ec9b781b949232a3cc8ff0397a8256c696ea6c7
David Haynes <david.e.haynes@stfc.ac.uk>
2013-11-01 15:36:01 +0000

    burst --dc works

--- cloudhands/burst/main.py
+++ cloudhands/burst/main.py
@@ -5,11 +5,34 @@ import argparse
 import logging
 import sys
 
+from libcloud import security
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+
 import cloudhands.burst
 import cloudhands.common
 
+from cloudhands.common.discovery import bundles
+from cloudhands.common.discovery import settings
+
 __doc__ = """
 """
+
+security.CA_CERTS_PATH = bundles
+
+
+def list_dc():
+    config = next(iter(settings))  # FIXME
+    user = config["user"]["name"]
+    pswd = config["user"]["pass"]
+    host = config["host"]["name"]
+    port = config["host"]["port"]
+    apiV = config["host"]["api_version"]
+    vcloudDrvr = get_driver(Provider.VCLOUD)
+    conn = vcloudDrvr(
+        user, pswd, host=host, port=port, api_version=apiV)
+    return conn.vdcs
+        
 
 def main(args):
     rv = 1
@@ -20,16 +43,26 @@ def main(args):
         for mod in (cloudhands.burst, cloudhands.common):
             log.info("{:18} version {}".format(mod.__name__, mod.__version__))
         rv = 0
+    elif args.dc:
+        for i in list_dc():
+            log.info (i)
     return rv
+
 
 def parser():
     rv = argparse.ArgumentParser(description=__doc__)
-    rv.add_argument("--version", action="store_true", default=False,
-    help="Print the current version number")
-    rv.add_argument("-v", "--verbose", required=False,
-    action="store_const", dest="log_level",
-    const=logging.DEBUG, default=logging.INFO,
-    help="Increase the verbosity of output")
+    rv.add_argument(
+        "--version", action="store_true", default=False,
+        help="Print the current version number")
+    rv.add_argument(
+        "-v", "--verbose", required=False,
+        action="store_const", dest="log_level",
+        const=logging.DEBUG, default=logging.INFO,
+        help="Increase the verbosity of output")
+    rv.add_argument(
+        "--dc", action="store_true", default=False,
+        help="Interact with data centres")
+    
     return rv
 
 def run():
