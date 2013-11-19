231f9ed23cb09331d390f834f30e91593ce3a6f4
David Haynes <david.e.haynes@stfc.ac.uk>
2013-11-19 12:02:11 +0000

    Burst demo uses futures. Unproven.

--- cloudhands/burst/demo.py
+++ cloudhands/burst/demo.py
@@ -2,7 +2,9 @@
 # encoding: UTF-8
 
 import argparse
+import concurrent.futures
 from configparser import ConfigParser
+import datetime
 import logging
 import sqlite3
 import sys
@@ -19,8 +21,10 @@ from cloudhands.common.connectors import Initialiser
 from cloudhands.common.connectors import Session
 from cloudhands.common.discovery import bundles
 from cloudhands.common.discovery import settings
+from cloudhands.common.fsm import HostState
 from cloudhands.common.schema import Component
 from cloudhands.common.schema import Host
+from cloudhands.common.schema import Node
 from cloudhands.common.schema import State
 from cloudhands.common.schema import Touch
 
@@ -34,6 +38,21 @@ DFLT_DB = ":memory:"
 
 security.CA_CERTS_PATH = bundles
 
+def create_node(name, auth, size=None, image=None):
+    """
+    Create a node the libcloud way. Connection is created locally to permit
+    threadpool dispatch.
+    """
+    conn = configure_driver()
+    img = image or next(
+        i for i in conn.list_images() if i.name == "Routed-Centos6.4a")
+    size = size or next(
+        i for i in conn.list_sizes() if i.name == "1024 Ram")
+    node = conn.create_node(name=name, auth=auth, size=size, image=image)
+    rv = vars(node)
+    del rv["driver"]
+    print(rv)
+    return rv
 
 def configure_driver():
     config = next(iter(settings))  # FIXME
@@ -57,6 +76,9 @@ def hosts(session, state=None):
 
 def main(args):
     rv = 1
+    logging.basicConfig(
+        level=args.log_level,
+        format="%(asctime)s %(levelname)-7s %(name)s|%(message)s")
     log = logging.getLogger("cloudhands.burst.demo")
 
     ldr = DemoLoader(config=ConfigParser(), path=args.db)
@@ -64,13 +86,34 @@ def main(args):
     user = ldr.grant_user_membership()
     ldr.load_hosts_for_user(user)
 
-    conn = configure_driver()
-    img = next(i for i in conn.list_images() if i.name == "Routed-Centos6.4a")
-    size = next(i for i in conn.list_sizes() if i.name == "1024 Ram")
-    for host in hosts(ldr.con.session, state="requested"):
-        pwd = NodeAuthPassword("q1W2e3R4t5Y6")
-        node = conn.create_node(name=host.name, auth=pwd, size=size, image=img)
-        print(node)
+    pwd = NodeAuthPassword("q1W2e3R4t5Y6")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as exctr:
+    #with concurrent.futures.ProcessPoolExecutor(max_workers=4) as exctr:
+        jobs = {exctr.submit(
+            create_node, name=host.name, auth=pwd): host for host in hosts(
+                ldr.con.session, state="requested")}
+
+        now = datetime.datetime.utcnow()
+        scheduling = ldr.con.session.query(HostState).filter(
+            HostState.name == "scheduling").one()
+        for host in jobs.values():
+            host.changes.append(
+                Touch(artifact=host, actor=user, state=scheduling, at=now))
+            ldr.con.session.commit()
+            log.info("{} is scheduling".format(host.name))
+
+        unknown = ldr.con.session.query(HostState).filter(
+            HostState.name == "unknown").one()
+        for job in concurrent.futures.as_completed(jobs):
+            host = jobs[job]
+            now = datetime.datetime.utcnow()
+            act = Touch(artifact=host, actor=user, state=unknown, at=now)
+            host.changes.append(act)
+            node = Node(name=host.name, touch=act)
+            ldr.con.session.add(node)
+            ldr.con.session.commit()
+            log.info("{}: Status unknown".format(host.name))
+
     return rv
 
 
