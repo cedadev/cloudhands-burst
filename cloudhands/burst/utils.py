093c04ef901941c9741fa244f15a3d0001074e3e
David Haynes <david.e.haynes@stfc.ac.uk>
2014-07-03 10:49:25 +0100

    Added unescape_script to utils.

--- cloudhands/burst/utils.py
+++ cloudhands/burst/utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: UTF-8
 
+import xml.sax.saxutils
+
 def find_xpath(xpath, tree, namespaces={}, **kwargs):
     elements = tree.iterfind(xpath, namespaces=namespaces)
     if not kwargs:
@@ -9,3 +11,7 @@ def find_xpath(xpath, tree, namespaces={}, **kwargs):
         query = set(kwargs.items())
         return (i for i in elements if query.issubset(set(i.attrib.items())))
 
+def unescape_script(text):
+    data = ''.join(text.splitlines())
+    return xml.sax.saxutils.unescape(
+        data, entities={"&quot;": '"', "&#13;": "\n"})
